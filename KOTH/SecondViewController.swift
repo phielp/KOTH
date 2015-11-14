e08de56bf3dc477ad6d3a5c4b461f1525ad7df23
phielp <phielp@gmail.com>
2015-11-14 03:48:59 +0100

    Updated sound + char info

--- KOTH/SecondViewController.swift
+++ KOTH/SecondViewController.swift
@@ -2,6 +2,8 @@
 //  SecondViewController.swift
 //  KOTH
 //
+//  Character screen.
+//
 //  Created by Philip Bouman on 13-11-15.
 //  Copyright © 2015 Philip Bouman. All rights reserved.
 //
@@ -11,27 +13,68 @@ import AVFoundation
 
 class SecondViewController: UIViewController {
     
-    var hank : [AnyObject] = ["hank.png"]
-    var peggy : [AnyObject] = ["peggy.jpg"]
-    var bobby : [AnyObject] = ["Bobby.png"]
-    var bill : [AnyObject] = ["Bill.jpg"]
-    var dale : [AnyObject] = ["Dale.jpg"]
-    var jeff : [AnyObject] = ["Boomhauer.jpg"]
+    // Character data, stored as:
+    //      image, soundbite1, soundbite2, name, age, job
+    var hank : [AnyObject] = ["hank.png", "Hank1", "Hank2", "Henry \"Hank\" Rutherford Hill", "43", "Assisted manager of Strickland Propane"]
+    var peggy : [AnyObject] = ["peggy.jpg", "Peggy1", "Peggy2", "Margaret J. \"Peggy\" Hill", "42", "Substitute Spanish Teacher"]
+    var bobby : [AnyObject] = ["Bobby.png", "Bobby1", "Bobby2", "Robert Jeffrey \"Bobby\" Hill", "13", "Student at Tom Landry Middle School"]
+    var bill : [AnyObject] = ["Bill.jpg", "Bill1", "Bill2", "William \"Bill\" Fontaine de la Tour Dauterive", "41", "Army Sergeant/ Barber"]
+    var dale : [AnyObject] = ["Dale.jpg", "Dale1", "Dale2", "Dale Alvin Gribble", "40", "Exterminator"]
+    var jeff : [AnyObject] = ["Boomhauer.jpg", "Boomhauer1", "Boomhauer2", "Jeff Boomhauer", "40", "Texas Ranger"]
     var currentChar : [AnyObject] = []
     
+    // ID of button clicked from ViewController (profiles screen)
     var toPass:Int!
     
-    @IBOutlet weak var charImage: UIImageView!
+    var quote1 : AVAudioPlayer?
+    var quote2 : AVAudioPlayer?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        print("\(toPass)")
-        getCharData(toPass)
-        displayProfile()
-        // load sounds
-        //        kothTune = setupAudioPlayerWithFile("kothTune", type:"wav")
+    // vote value
+    var rating : Int = 1
+
+    @IBOutlet weak var charImage: UIImageView!
+    @IBOutlet weak var charText: UITextView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    // confirm vote
+    @IBAction func voteButton(sender: AnyObject) {
+        print("\(rating)")
+    }
+
+    // set vote value
+    @IBAction func indexChanged(sender: AnyObject) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            rating = 1
+        case 1:
+            rating = 2
+        case 2:
+            rating = 3
+        case 3:
+            rating = 4
+        case 4:
+            rating = 5
+        default:
+            rating = 1
+        }
     }
     
+    // quote button 1
+    @IBAction func quoteButton1(sender: AnyObject) {
+        quote1?.play()
+    }
+    
+    // quote button 2
+    @IBAction func quoteButton2(sender: AnyObject) {
+        quote2?.play()
+    }
+    
+    // load view
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getCharData(toPass)
+        displayProfile()
+    }
     
     // get character data
     func getCharData(tag: Int) {
@@ -53,15 +96,49 @@ class SecondViewController: UIViewController {
         }
     }
     
-    // 
+    // connect all data to profile
     func displayProfile() {
-        print("\(currentChar[0])")
         loadImage()
+        loadSound()
+        loadText()
         currentChar = []
     }
     
+    // load profile pic
     func loadImage() {
         let newImg: UIImage? = UIImage(named: currentChar[0] as! String)
         self.charImage.image = newImg
     }
+    
+    // load soundbites
+    func loadSound() {
+        quote1 = setupAudioPlayerWithFile(currentChar[1] as! String, type:"aif")
+        quote2 = setupAudioPlayerWithFile(currentChar[2] as! String, type:"aif")
+    }
+    
+    // load char info
+    // Multiple fonts, indentation?
+    func loadText() {
+        charText.text = "NAME: \(currentChar[3])\u{0085}AGE:    \(currentChar[4])\u{0085}JOB:    \(currentChar[5])"
+    }
+    
+    // Setup audioplayer and load files
+    func setupAudioPlayerWithFile(file:String, type:String) -> AVAudioPlayer?  {
+        // setup path
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
+        let url = NSURL.fileURLWithPath(path!)
+        
+        var audioPlayer:AVAudioPlayer?
+        
+        // setup player
+        do {
+            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
+        } catch {
+            print("Player not available")
+        }
+        
+        audioPlayer?.prepareToPlay()
+        return audioPlayer
+    }
+
 }
--- KOTH/ViewController.swift
+++ KOTH/ViewController.swift
@@ -1,6 +1,8 @@
 //
 //  ViewController.swift
 //  KOTH
+//  
+//  Profile screen.
 //
 //  Created by Philip Bouman on 09-11-15.
 //  Copyright © 2015 Philip Bouman. All rights reserved.
@@ -10,62 +12,29 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    
-//    var kothTune : AVAudioPlayer?   // music
 
-    
+    // character buttons
     @IBAction func charButtons(sender: AnyObject) {
-        print("\(sender.tag)")
+
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // load sounds
-//        kothTune = setupAudioPlayerWithFile("kothTune", type:"wav")
+
     }
 
-
-    
-    
     // send tag value to secondviewcontroller (svc)
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "charInfo") {
             let svc = segue.destinationViewController as! SecondViewController;
             
             svc.toPass = sender.tag
-            
         }
     }
 
-    
-    
-    
-    // Setup audioplayer and load files
-    func setupAudioPlayerWithFile(file:NSString, type:NSString) -> AVAudioPlayer?  {
-        // setup path
-        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: type as String)
-        let url = NSURL.fileURLWithPath(path!)
-        
-        var audioPlayer:AVAudioPlayer?
-        
-        // setup player
-        do {
-            try audioPlayer = AVAudioPlayer(contentsOfURL: url)
-        } catch {
-            print("Player not available")
-        }
-        
-        audioPlayer?.prepareToPlay()
-        return audioPlayer
-    }
-
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
